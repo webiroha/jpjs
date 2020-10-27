9711d374b6cd1f4ff10774365893a420b9278a6e
Keiko <24987971+webiroha@users.noreply.github.com>
2020-10-27 18:00:48 +0900

    on the way to character page

--- src/app/characters/index.js
+++ src/app/characters/index.js
@@ -5,15 +5,40 @@ import Frag from '../components/atom/Frag';
 import Element from '../components/atom/Element';
 import Center from '../components/layout/Center';
 
-const { root, WholeFrag, Wrapper, CenterFrag } = Center();
+const { root, WholeFrag, Wrapper } = Center();
+
+const ExplainFrag = Frag();
+
+const ExplainBlock = Element({
+  elem: 'div',
+  class: 'explain-block',
+});
 
 const Title = Element({
   elem: 'h1',
-  class: 'title',
+  class: 'title slide-in-upper__0',
   text: 'Japanese characters',
 });
 
-root.appendChild(Title.tag);
+const Explain = Element({
+  elem: 'p',
+  class: 'text slide-in-upper__1',
+  text: 'Japanese has three kind of characters.',
+});
+
+const Characters = Element({
+  elem: 'p',
+  class: 'characters',
+  text: 'Hiragana(ひらがな), Katakana(カタカナ) and Kanji(漢字).',
+});
+
+ExplainFrag().appendChild(Title.tag);
+ExplainFrag().appendChild(Explain.tag);
+ExplainFrag().appendChild(Characters.tag);
+ExplainBlock.tag.appendChild(ExplainFrag());
+Wrapper.tag.appendChild(ExplainBlock.tag);
+
+root.appendChild(Wrapper.tag);
 
 // const characters = [
 //   { Hiragana: 'ひらがな' },
