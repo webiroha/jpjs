c55fd095d9c02e062af89982c83d091ae4342cb1
Keiko <24987971+webiroha@users.noreply.github.com>
2020-11-16 15:13:40 +0900

    remove unnecessary webpack plugin

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,7 +1,5 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-// const TerserPlugin = require('terser-webpack-plugin');
 const webpackBaseConfig = require('./webpack.common.config.js');
 
 module.exports = merge(webpackBaseConfig, {
@@ -10,21 +8,5 @@ module.exports = merge(webpackBaseConfig, {
     path: path.resolve(__dirname, '../docs'),
     publicPath: '/jpjs/',
     filename: '[name].[chunkhash].js',
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new OptimizeCSSAssetsPlugin(),
-      // new TerserPlugin({
-      //   terserOptions: {
-      //     ecma: 6,
-      //     compress: true,
-      //     output: {
-      //       comments: false,
-      //       beautify: false,
-      //     },
-      //   },
-      // }),
-    ],
   },
 });
