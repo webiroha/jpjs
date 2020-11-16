7f29a2c708667e71d6b1a888e4f9b2c2f3d07651
Keiko <24987971+webiroha@users.noreply.github.com>
2020-11-16 17:19:00 +0900

    refactor webpack

--- config/webpack.common.config.js
+++ config/webpack.common.config.js
@@ -5,7 +5,31 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const GoogleFontsPlugin = require('google-fonts-webpack-plugin');
 
-module.exports = {
+const pageMetas = {
+  top: {
+    title: 'Japanese JavaScript',
+    description:
+      'Play with Japanese & JavaScript. This place will be not only a personal JavaScript playground but also a combined place for someone to be able to learn Japanese a little.',
+  },
+
+  preparation: {
+    title: 'Japanese JavaScript | Preparation',
+    description:
+      'This page introduces how to change keyboard settings for typing Japanese.',
+  },
+
+  characters: {
+    title: 'Japanese JavaScript | Characters',
+    description: 'There are three kind of characters in Japanese.',
+  },
+
+  vowels: {
+    title: 'Japanese JavaScript | Vowels',
+    description: 'Japanese has 5 vowels.',
+  },
+};
+
+const webpackConfig = {
   entry: {
     top: './src/app/index.js',
     preparation: './src/app/intro/preparation/index.js',
@@ -70,56 +94,7 @@ module.exports = {
   },
 
   plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Japanese JavaScript',
-      description:
-        'Play with Japanese & JavaScript. This place will be not only a personal JavaScript playground but also a combined place for someone to be able to learn Japanese a little.',
-      filename: 'index.html',
-      template: './src/template/index.html',
-      inject: true,
-      minify: {
-        removeComments: true,
-        collapseWhitespace: false,
-      },
-      chunks: ['top'],
-    }),
-    new HtmlWebpackPlugin({
-      title: 'Japanese JavaScript | Preparation',
-      description:
-        'This page introduces how to change keyboard settings for typing Japanese.',
-      filename: 'intro/preparation/index.html',
-      template: './src/template/index.html',
-      inject: true,
-      minify: {
-        removeComments: true,
-        collapseWhitespace: false,
-      },
-      chunks: ['preparation'],
-    }),
-    new HtmlWebpackPlugin({
-      title: 'Japanese JavaScript | Characters',
-      description: 'There are three kind of characters in Japanese.',
-      filename: 'intro/characters/index.html',
-      template: './src/template/index.html',
-      inject: true,
-      minify: {
-        removeComments: true,
-        collapseWhitespace: false,
-      },
-      chunks: ['characters'],
-    }),
-    new HtmlWebpackPlugin({
-      title: 'Japanese JavaScript | Vowels',
-      description: 'Japanese has 5 vowels.',
-      filename: 'intro/vowels/index.html',
-      template: './src/template/index.html',
-      inject: true,
-      minify: {
-        removeComments: true,
-        collapseWhitespace: false,
-      },
-      chunks: ['vowels'],
-    }),
+    new HtmlWebpackPlugin(),
     new MiniCssExtractPlugin({
       filename: 'style.[chunkhash].css',
     }),
@@ -132,3 +107,22 @@ module.exports = {
     new CleanWebpackPlugin(),
   ],
 };
+
+Object.keys(webpackConfig.entry).forEach((key) => {
+  webpackConfig.plugins.push(
+    new HtmlWebpackPlugin({
+      title: pageMetas[key].title,
+      description: pageMetas[key].description,
+      filename: key === 'top' ? 'index.html' : `intro/${key}/index.html`,
+      template: './src/template/index.html',
+      inject: true,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: false,
+      },
+      chunks: [key],
+    })
+  );
+});
+
+module.exports = webpackConfig;
