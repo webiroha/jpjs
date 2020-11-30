5b9b4b45f72919d077bc1492fd6a2ea19ad84852
Keiko <24987971+webiroha@users.noreply.github.com>
2020-11-30 16:22:46 +0900

    on the way to create consonant page

---
+++ src/app/components/SubTitleBlock/index.js
@@ -0,0 +1,31 @@
+import Frag from '../atom/Frag';
+import Element from '../atom/Element';
+
+const SubTitleBlock = (props) => {
+  const SubTitleBlockFrag = Frag();
+
+  const Wrapper = Element({
+    elem: 'div',
+    class: 'subtitle-block',
+  });
+
+  const Title = Element({
+    elem: 'h2',
+    class: 'subtitle-block__title slide-in-upper__0',
+    text: props.title,
+  });
+
+  const Text = Element({
+    elem: 'p',
+    class: 'text opacity_0 slide-in-upper__1',
+    text: props.text,
+  });
+
+  SubTitleBlockFrag().appendChild(Title.tag);
+  SubTitleBlockFrag().appendChild(Text.tag);
+  Wrapper.tag.appendChild(SubTitleBlockFrag());
+
+  return () => Wrapper.tag;
+};
+
+export default SubTitleBlock;
--- src/app/intro/consonants/index.js
+++ src/app/intro/consonants/index.js
@@ -2,6 +2,7 @@ import HeaderFooter from '@root/components/pages/HeaderFooter';
 import Frag from '@root/components/atom/Frag';
 import Element from '@root/components/atom/Element';
 import Explain from '@root/components/Explain';
+import SubTitleBlock from '@root/components/SubTitleBlock';
 import CodeBlock from '@root/components/CodeBlock';
 import NextLink from '@root/components/links/NextLink';
 
@@ -12,6 +13,7 @@ const Consonants = () => {
     title: 'Consonants',
     text: 'Japanese has around 14 - 16 consonants.',
   };
+
   const ConsonantsExplainSmall = Element({
     elem: 'small',
     class: 'small slide-in-upper__1',
@@ -54,150 +56,18 @@ const Consonants = () => {
     vowels.map((vowel) => consonant + vowel)
   );
   console.log(...roughlyCombinedSounds);
-  // ...roughlyCombinedSounds !== Romaji
 
-  //   const SoundButtonText = Element({
-  //     elem: 'p',
-  //     class: 'text opacity_0 slide-in-upper__1',
-  //     text: "Let's hear the sound!",
-  //   });
-  //   ContentsFrag().appendChild(SoundButtonText.tag);
+  const SubTitleInfo = {
+    title: 'Each Consonant',
+    text: "Let's learn each consonant deeply!",
+  };
 
-  //   const consonants = ['a', 'i', 'u', 'e', 'o'];
+  const SubTitleInfoBlock = SubTitleBlock(SubTitleInfo);
 
-  //   const SoundsBlock = Element({
-  //     elem: 'div',
-  //     class: 'sound-block fade-in',
-  //   });
-  //   const SoundsFrag = Frag();
-
-  //   const createSoundButtons = (item) => {
-  //     const sound = new Audio(`../../assets/sounds/single/${item}.wav`);
-
-  //     const SoundButton = Element({
-  //       elem: 'button',
-  //       class: 'sound-button',
-  //       text: item,
-  //     });
-  //     SoundButton.tag.type = 'button';
-  //     SoundButton.tag.addEventListener('click', () => {
-  //       sound.currentTime = 0;
-  //       sound.play();
-  //     });
-
-  //     SoundsFrag().appendChild(SoundButton.tag);
-  //   };
-
-  //   consonants.map((i) => createSoundButtons(i));
-
-  //   SoundsBlock.tag.appendChild(SoundsFrag());
-  //   ContentsFrag().appendChild(SoundsBlock.tag);
-
-  //   const VowelHiraganaKatakana = Element({
-  //     elem: 'p',
-  //     class: 'text opacity_0 slide-in-upper__1',
-  //     text: 'The consonants spelt by hiragana and katakana.',
-  //   });
-  //   ContentsFrag().appendChild(VowelHiraganaKatakana.tag);
-
-  //   const consonantLetters = `const consonantLetters = [
-  //     { a: { hiragana: 'あ', katakana: 'ア' } },
-  //     { i: { hiragana: 'い', katakana: 'イ' } },
-  //     { u: { hiragana: 'う', katakana: 'ウ' } },
-  //     { e: { hiragana: 'え', katakana: 'エ' } },
-  //     { o: { hiragana: 'お', katakana: 'オ' } },
-  //   ];`;
-
-  //   const { Code: VowelLettersCode } = CodeBlock(consonantLetters);
-  //   ContentsFrag().appendChild(VowelLettersCode.tag);
-
-  //   const ExampleText = Element({
-  //     elem: 'p',
-  //     class: 'text opacity_0 slide-in-upper__1',
-  //     text: "Let's use consonants in practise!",
-  //   });
-  //   ContentsFrag().appendChild(ExampleText.tag);
-
-  //   const examples = `const consonants =
-  //   { a: 'あ', i: 'い', u: 'う', e: 'え', o: 'お' };
-
-  // const englishHiragana = (
-  //   english,
-  //   hiragana
-  // ) =>
-  //   \`\${english} is \${hiragana} in Hiragana.\`;
-
-  // const hiraganaKanji = (
-  //   hiragana,
-  //   kanji
-  // ) =>
-  //   \`\${hiragana} is \${kanji} in Kanji.\`;
-
-  // const love = consonants.a + consonants.i;
-  // const loveHiragana = englishHiragana('Love', love);
-  // const loveKanji = hiraganaKanji(love, '愛');
-
-  // console.log(loveHiragana);
-  // // Love is あい in Hiragana.
-  // console.log(loveKanji);
-  // // あい is 愛 in Kanji.
-
-  // const no = consonants.i.repeat(2) + consonants.e;
-  // const noHiragana = englishHiragana('No', no);
-
-  // console.log(noHiragana);
-  // // No is いいえ in Hiragana.
-
-  // const up = consonants.u + consonants.e;
-  // const upHiragana = englishHiragana('Up', up);
-  // const upKanji = hiraganaKanji(up, '上');
-
-  // console.log(upHiragana);
-  // // Up is うえ in Hiragana.
-  // console.log(upKanji);
-  // // うえ is 上 in Kanji.`;
-
-  //   const { Code: ExamplesCode } = CodeBlock(examples);
-  //   ContentsFrag().appendChild(ExamplesCode.tag);
-
+  ContentsFrag().appendChild(SubTitleInfoBlock());
   ContentsFrag().appendChild(NextLink(''));
 
   return ContentsFrag();
 };
 
 HeaderFooter(Consonants);
-
-// // for displaying console.log
-// const consonants = { a: 'あ', i: 'い', u: 'う', e: 'え', o: 'お' };
-
-// const englishHiragana = (english, hiragana) =>
-//   `${english} is ${hiragana} in Hiragana.`;
-// const hiraganaKanji = (hiragana, kanji) => `${hiragana} is ${kanji} in Kanji.`;
-
-// const love = consonants.a + consonants.i;
-// const loveHiragana = englishHiragana('Love', love);
-// const loveKanji = hiraganaKanji(love, '愛');
-
-// console.log(loveHiragana);
-// // Love is あい in Hiragana.
-// console.log(loveKanji);
-// // あい is 愛 in Kanji.
-
-// const no = consonants.i.repeat(2) + consonants.e;
-// const noHiragana = englishHiragana('No', no);
-
-// console.log(noHiragana);
-// // No is いいえ in Hiragana.
-
-// const up = consonants.u + consonants.e;
-// const upHiragana = englishHiragana('Up', up);
-// const upKanji = hiraganaKanji(up, '上');
-
-// console.log(upHiragana);
-// // Up is うえ in Hiragana.
-// console.log(upKanji);
-// // うえ is 上 in Kanji.
-
-// const top = [{ romaji: 'ue', hiragana: 'うえ', kanji: '上' }];
-// const house = [{ romaji: 'ie', hiragana: 'いえ', kanji: '家' }];
-// const blue = [{ romaji: 'ao', hiragana: 'あお', kanji: '青' }];
