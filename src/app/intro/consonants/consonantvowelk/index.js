5b2a2978648ec29fb93f0165ce77b7d074dabbda
Keiko <24987971+webiroha@users.noreply.github.com>
2020-12-08 16:01:08 +0900

    separate info from consonantvowelk page

--- src/app/intro/consonants/consonantvowelk/index.js
+++ src/app/intro/consonants/consonantvowelk/index.js
@@ -1,4 +1,5 @@
 import CommonLayout from '@root/intro/consonants/components/CommonLayout';
+import info from '@root/intro/consonants/info';
 
 import '@wav/ka.wav';
 import '@wav/ki.wav';
@@ -6,51 +7,7 @@ import '@wav/ku.wav';
 import '@wav/ke.wav';
 import '@wav/ko.wav';
 
-const consonantKinfo = {
-  explain: {
-    title: 'Consonant K',
-    text: "Let's try to learn consonant k + vowels!",
-  },
-  romaji: `  const consonantK = 'k';
-    const vowels = [...'aiueo'];
-    const consonantKvowelsByRomaji = vowels.map(
-      (vowel) => consonantK + vowel
-    );
-    console.log(consonantKvowelsByRomaji.toString());
-    // ka,ki,ku,ke,ko`,
-  sound: ['ka', 'ki', 'ku', 'ke', 'ko'],
-  jpText: 'The consonant k + vowels spelt by hiragana and katakana.',
-  letters: `const kvowelLetters = [
-      { ka: { hiragana: 'か', katakana: 'カ' } },
-      { ki: { hiragana: 'き', katakana: 'キ' } },
-      { ku: { hiragana: 'く', katakana: 'ク' } },
-      { ke: { hiragana: 'け', katakana: 'ケ' } },
-      { ko: { hiragana: 'こ', katakana: 'コ' } },
-    ];`,
-  exampleText: "Let's use consonant k + vowels in practice!",
-  examples: `// Base code
-    const vowels =
-      { a: 'あ', i: 'い', u: 'う', e: 'え', o: 'お' };
-
-    const kvowels =
-      { ka: 'か', ki: 'き', ku: 'く', ke: 'け', ko: 'こ' };
-
-    const englishHiragana = (
-      english,
-      hiragana
-    ) =>
-      \`\${english} is \${hiragana} in Hiragana.\`;
-
-    const hiraganaKanji = (
-      hiragana,
-      kanji
-    ) =>
-      \`\${hiragana} is \${kanji} in Kanji.\`;
-    `,
-  navLink: ['consonants', '', 'one', ''],
-};
-
-CommonLayout(consonantKinfo);
+CommonLayout(info.consonantKinfo);
 
 const consonantK = 'k';
 const vowels = [...'aiueo'];
---
+++ src/app/intro/consonants/info/index.js
@@ -0,0 +1,47 @@
+const info = {
+  consonantKinfo: {
+    explain: {
+      title: 'Consonant K',
+      text: "Let's try to learn consonant k + vowels!",
+    },
+    romaji: `  const consonantK = 'k';
+    const vowels = [...'aiueo'];
+    const consonantKvowelsByRomaji = vowels.map(
+      (vowel) => consonantK + vowel
+    );
+    console.log(consonantKvowelsByRomaji.toString());
+    // ka,ki,ku,ke,ko`,
+    sound: ['ka', 'ki', 'ku', 'ke', 'ko'],
+    jpText: 'The consonant k + vowels spelt by hiragana and katakana.',
+    letters: `const kvowelLetters = [
+      { ka: { hiragana: 'か', katakana: 'カ' } },
+      { ki: { hiragana: 'き', katakana: 'キ' } },
+      { ku: { hiragana: 'く', katakana: 'ク' } },
+      { ke: { hiragana: 'け', katakana: 'ケ' } },
+      { ko: { hiragana: 'こ', katakana: 'コ' } },
+    ];`,
+    exampleText: "Let's use consonant k + vowels in practice!",
+    examples: `// Base code
+    const vowels =
+      { a: 'あ', i: 'い', u: 'う', e: 'え', o: 'お' };
+
+    const kvowels =
+      { ka: 'か', ki: 'き', ku: 'く', ke: 'け', ko: 'こ' };
+
+    const englishHiragana = (
+      english,
+      hiragana
+    ) =>
+      \`\${english} is \${hiragana} in Hiragana.\`;
+
+    const hiraganaKanji = (
+      hiragana,
+      kanji
+    ) =>
+      \`\${hiragana} is \${kanji} in Kanji.\`;
+    `,
+    navLink: ['consonants', '', 'one', ''],
+  },
+};
+
+export default info;
