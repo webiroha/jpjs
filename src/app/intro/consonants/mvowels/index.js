0abdc4bc7240e4058723ba88232671ae710c5bff
webiroha <24987971+webiroha@users.noreply.github.com>
2021-01-03 16:14:01 +0900

    create mvowels page base

---
+++ src/app/intro/consonants/mvowels/index.js
@@ -0,0 +1,15 @@
+import CommonLayout from '@root/intro/consonants/components/CommonLayout';
+import info from '@root/intro/consonants/info';
+
+import '@wav/ma.wav';
+import '@wav/mi.wav';
+import '@wav/mu.wav';
+import '@wav/me.wav';
+import '@wav/mo.wav';
+
+// CommonLayout(info.consonantMinfo);
+
+const consonantM = 'm';
+const vowels = [...'aiueo'];
+const consonantMvowelsByRomaji = vowels.map((vowel) => consonantM + vowel);
+console.log(consonantMvowelsByRomaji.toString());
