0f4a880d79478a4807db48a1c0277567abe5553b
webiroha <24987971+webiroha@users.noreply.github.com>
2021-01-11 17:22:06 +0900

    add note to yvowels

--- src/app/intro/consonants/yvowels/index.js
+++ src/app/intro/consonants/yvowels/index.js
@@ -14,3 +14,14 @@ const consonantYvowelsByRomaji = vowels
   .filter((_, i) => i % 2 === 0);
 console.log(consonantYvowelsByRomaji.toString());
 // ya, yu, yo
+
+// There might be a time you see such the way to write below.
+// The 'i' and 'e' is the same as vowel's one and most Japanese might not be sure when to use😅
+const consonantYvowelsSeldom = vowels.map((vowel) => {
+  if (vowel === 'i') return vowel;
+  else if (vowel === 'e') return vowel;
+  else return consonantY + vowel;
+});
+
+console.log(consonantYvowelsSeldom.toString());
+// ya, i, yu, e, yo
