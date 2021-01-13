b913250bab1b25b292004b6bffde79b6cd206183
webiroha <24987971+webiroha@users.noreply.github.com>
2021-01-13 14:31:20 +0900

    on the way to create contents to wvowels

--- src/app/intro/consonants/wvowels/index.js
+++ src/app/intro/consonants/wvowels/index.js
@@ -8,13 +8,22 @@ CommonLayout(info.consonantYinfo);
 
 const consonantW = 'w';
 const vowels = [...'aiueo'];
-const consonantYvowelsByRomaji = [consonantW + vowels[0], 'o'];
-console.log(consonantYvowelsByRomaji.toString());
-// wa, o
 
-const consonantWvowelsNihon = vowels
+// Nihon-shiki Romaji
+const wVowelsByRomajiNihon = vowels
   .filter((vowel) => vowel === 'a' || vowel === 'o')
   .map((vowel) => consonantW + vowel);
 
-console.log(consonantWvowelsNihon.toString());
+console.log(wVowelsByRomajiNihon.toString());
 // wa, wo
+
+// Kunrei-shiki and Hepburn-shiki Romaji
+// (We will use this here.)
+const wVowelsByRomajiKunrei = [consonantW + vowels[0], 'o'];
+console.log(wVowelsByRomajiKunrei.toString());
+// wa, o
+
+// There might be a time you see such the ways to write below.
+// The sounds of 'i'/'u'/'e'/'wi'/'we' are the same as vowels'
+// but those letters of Hiragana/Katakana are different.
+// these are old letters.
