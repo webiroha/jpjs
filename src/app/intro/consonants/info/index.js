b22a7fff6057a32ba4f880ed00f7ade800998969
webiroha <24987971+webiroha@users.noreply.github.com>
2021-01-19 15:32:04 +0900

    create gojuon content

--- src/app/intro/consonants/info/index.js
+++ src/app/intro/consonants/info/index.js
@@ -1234,3 +1234,18 @@ const helloHiragana = englishHiragana('Hello', hello);
 
 console.log(helloHiragana);
 // Hello is こんにちは in Hiragana.
+
+// Those are called as Gojuon/ごじゅうおん/五十音
+const gojuon = [
+  { a: 'あ', i: 'い', u: 'う', e: 'え', o: 'お' },
+  { ka: 'か', ki: 'き', ku: 'く', ke: 'け', ko: 'こ' },
+  { sa: 'さ', shi: 'し', su: 'す', se: 'せ', so: 'そ' },
+  { ta: 'た', chi: 'ち', tsu: 'つ', te: 'て', to: 'と' },
+  { na: 'な', ni: 'に', nu: 'ぬ', ne: 'ね', no: 'の' },
+  { ha: 'は', hi: 'ひ', fu: 'ふ', he: 'へ', ho: 'ほ' },
+  { ma: 'ま', mi: 'み', mu: 'む', me: 'め', mo: 'も' },
+  { ya: 'や', yu: 'ゆ', yo: 'よ' },
+  { ra: 'ら', ri: 'り', ru: 'る', re: 'れ', ro: 'ろ' },
+  { wa: 'わ', o: 'を' },
+  { n: 'ん' },
+];
