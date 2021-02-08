ac11c5a8ef8b1c83d12a695b88d40dbf000c868d
webiroha <24987971+webiroha@users.noreply.github.com>
2021-02-08 16:23:18 +0900

    on the way to adding zvowels page

---
+++ src/app/intro/consonants/zvowels/index.js
@@ -0,0 +1,33 @@
+import CommonLayout from '@root/intro/consonants/components/CommonLayout';
+import info from '@root/intro/consonants/info';
+
+import '@wav/sa.wav';
+import '@wav/shi.wav';
+import '@wav/su.wav';
+import '@wav/se.wav';
+import '@wav/so.wav';
+
+CommonLayout(info.consonantSinfo);
+
+const [consonantZ, vowels, hepburnElem] = ['z', [...'aiueo'], 'j'];
+
+// const consonantS = 's';
+// const vowels = [...'aiueo'];
+// const hepburnElem = 'h';
+
+// Kunrei-shiki and Nihon-shiki Romaji
+const sVowelsByRomaji = vowels.map((vowel) => consonantS + vowel);
+console.log(sVowelsByRomaji.toString());
+// sa,si,su,se,so
+
+// Hepburn-shiki Romaji
+// (We will use this here.)
+const sVowelsByRomajiHepburn = vowels.map((vowel) => {
+  if (vowel === 'i') {
+    return consonantS + hepburnElem + vowel;
+  } else {
+    return consonantS + vowel;
+  }
+});
+console.log(sVowelsByRomajiHepburn.toString());
+// sa,shi,su,se,so
