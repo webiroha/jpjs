9d4f3da416ed924166b922d98899c3a560e41603
webiroha <24987971+webiroha@users.noreply.github.com>
2021-02-25 14:26:06 +0900

    modify shvowels

--- src/app/intro/consonants/shvowels/index.js
+++ src/app/intro/consonants/shvowels/index.js
@@ -12,12 +12,14 @@ const kunreiNihonElem = 'y';
 const hepburnElem = 'h';
 const vowels = [...'aiueo'];
 
+// Kunrei-shiki and Nihon-shiki Romaji
 const shVowelsByRomaji = vowels
   .filter((_, i) => i % 2 === 0)
   .map((vowel) => consonantS + kunreiNihonElem + vowel);
 console.log(shVowelsByRomaji.toString());
 // sya, syu, syo
 
+// Hepburn-shiki Romaji(We will use this here.)
 const shVowelsByRomajiHepburn = vowels
   .filter((_, i) => i % 2 === 0)
   .map((vowel) => consonantS + hepburnElem + vowel);
